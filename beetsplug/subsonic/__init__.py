4cfac7e6e71cd231ae290d638063c664cbb987c2
Dang Mai <contact@dangmai.net>
2016-11-15 22:59:43 -0500

    Refactor common endpoint

--- beetsplug/subsonic/__init__.py
+++ beetsplug/subsonic/__init__.py
@@ -1,20 +1,26 @@
+import pyxb.utils.domutils
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand
+from flask import Blueprint
 from flask import Flask
+
 from generated import api
-import pyxb.utils.domutils
+
 pyxb.utils.domutils.BindingDOMSupport.SetDefaultNamespace(api.Namespace)
 
-app = Flask(__name__)
+rest_api = Blueprint('rest_api', __name__)
 
 
-@app.route("/rest/ping.view")
+@rest_api.route("/ping.view")
 def ping():
     response = api.subsonic_response()
     response.version = api.Version('1.14.0')
     response.status = api.ResponseStatus.ok
     return response.toxml('utf-8'), 200, {'Content-Type': 'application/xml; charset=utf-8'}
 
+
+app = Flask(__name__)
+app.register_blueprint(rest_api, url_prefix='/rest')
 
 def init_server(lib, opts, args):
     app.run(debug=True)
