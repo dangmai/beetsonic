3d4ab01dc347c610f592353aee476cd8a287db59
Dang Mai <contact@dangmai.net>
2016-11-15 23:55:44 -0500

    Refactor initial response creation

--- beetsplug/subsonic/__init__.py
+++ beetsplug/subsonic/__init__.py
@@ -1,7 +1,9 @@
+from functools import wraps
+
 import pyxb.utils.domutils
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand
-from flask import Blueprint, jsonify
+from flask import Blueprint
 from flask import Flask
 from flask import Response
 
@@ -23,14 +25,24 @@ class SubsonicResponse(Response):
             return cls(response)
         return super(Response, cls).force_type(response, environ)
 
+
+def subsonic(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        response = api.subsonic_response()
+        response.version = api.Version('1.14.0')
+        response.status = api.ResponseStatus.ok
+        return func(response, *args, **kwargs)
+
+    return decorated_function
+
+
 rest_api = Blueprint('rest_api', __name__)
 
 
 @rest_api.route("/ping.view")
-def ping():
-    response = api.subsonic_response()
-    response.version = api.Version('1.14.0')
-    response.status = api.ResponseStatus.ok
+@subsonic
+def ping(response):
     return response
 
 
@@ -41,6 +53,8 @@ app.register_blueprint(rest_api, url_prefix='/rest')
 
 def init_server(lib, opts, args):
     app.run(debug=True)
+
+
 subsonic_cmd = Subcommand('subsonic', help='Run Subsonic server')
 subsonic_cmd.func = init_server
 
